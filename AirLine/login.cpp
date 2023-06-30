66acf4f10fe73650dd02501109557ab81ccb3850
Ahmed Sameh Emam <54156393+ahmedfox1@users.noreply.github.com>
2023-06-30 16:06:48 +0300

    Add project files.

---
+++ AirLine/login.cpp
@@ -0,0 +1,11 @@
+#include "login.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew AirLine::login);
+
+}
---
+++ AirLine/login.h
@@ -0,0 +1,68 @@
+#pragma once
+
+namespace AirLine {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for login
+	/// </summary>
+	public ref class login : public System::Windows::Forms::Form
+	{
+	public:
+		login(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~login()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// login
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1264, 681);
+			this->Name = L"login";
+			this->Text = L"login";
+			this->Load += gcnew System::EventHandler(this, &login::login_Load);
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void login_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	};
+}
